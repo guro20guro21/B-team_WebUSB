587f5158a53e6731ed73c30b64a4d0e86263a972
guro20guro21 <76155132+guro20guro21@users.noreply.github.com>
2021-07-12 22:03:46 +0900

    Add files via upload

---
+++ microbit/main.cpp
@@ -0,0 +1,40 @@
+#include<MicroBit.h>
+
+MicroBit uBit;
+ManagedString result("");
+
+void receive(){
+        while(true){
+                // 固定長ではなく現在バッファにあるデータ長を読み取るように変更
+                // これでASYNCでもSYNC_SLEEPでもどちらでも正しく動く
+                result = result + uBit.serial.read(uBit.serial.rxBufferedSize(), SYNC_SLEEP);                // 譲らないとコンテクストが切り替わらないことに注意
+                fiber_sleep(1);
+        }
+}
+
+void send(){
+        while(true){
+                while(! uBit.buttonA.isPressed()) fiber_sleep(100);
+                uBit.serial.printf("received:%d characters\n", result.length());
+                uBit.serial.send(result, SYNC_SLEEP);
+                // チャタリング防止
+                fiber_sleep(1000);
+        }
+}
+
+void clear(){
+        while(true){
+                while(! uBit.buttonB.isPressed()) fiber_sleep(100);
+                result = "";
+        }
+}
+
+int main(){
+        uBit.init();
+        uBit.serial.setBaud(9600);
+        uBit.serial.setRxBufferSize(255);
+        create_fiber(receive);
+        create_fiber(send);
+        create_fiber(clear);
+        release_fiber();
+}
