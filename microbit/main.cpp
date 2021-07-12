188c31ac185c13e7c6267e6822b2fbbaa772ec9a
Morimoto <37905975+Morimoto0925@users.noreply.github.com>
2021-07-13 02:07:36 +0900

    Update main.cpp

--- microbit/main.cpp
+++ microbit/main.cpp
@@ -6,14 +6,9 @@ ManagedString str("");
 
 void receive(){
         while(true){
-                // 固定長ではなく現在バッファにあるデータ長を読み取るように変更
-                // これでASYNCでもSYNC_SLEEPでもどちらでも正しく動く
                 str = uBit.serial.read(18, SYNC_SLEEP);
                 //int checksum = str.charAt(16);
                 result = result + str.substring(0,16);
-                //result = result + uBit.serial.read(uBit.serial.rxBufferedSize(), SYNC_SLEEP);
-                //result = result + uBit.serial.read(SYNC_SLEEP);
-                // 譲らないとコンテクストが切り替わらないことに注意
                 fiber_sleep(1);
         }
 }
@@ -23,7 +18,6 @@ void send(){
                 while(! uBit.buttonA.isPressed()) fiber_sleep(100);
                 uBit.serial.printf("received:%d characters\n", result.length());
                 uBit.serial.send(result, SYNC_SLEEP);
-                // チャタリング防止
                 fiber_sleep(1000);
         }
 }
@@ -38,7 +32,6 @@ void clear(){
 int main(){
         uBit.init();
         uBit.serial.setBaud(9600);
-        uBit.serial.setRxBufferSize(255);
         create_fiber(receive);
         create_fiber(send);
         create_fiber(clear);
